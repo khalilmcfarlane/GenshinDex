3a76451c7d24c0b36bb5d8a16168688bdfccca91
Khalil McFarlane <kmac36960@gmail.com>
2024-09-22 19:04:30 -0700

    Added basic code to display all characters on front page

--- app/js/genshin.js
+++ app/js/genshin.js
@@ -1,5 +1,6 @@
 class GenshinApi {
     #url = "https://gsi.fly.dev/";
+    body = document.body;
     constructor() {
     }
 
@@ -8,7 +9,61 @@ class GenshinApi {
     }
 
     async getAllCharacters() {
-        ///
+        try {
+            let response = await fetch(`https://genshin.jmp.blue/characters/all?lang=en`);
+            let data = await response.json();
+            console.log(data);
+            // add a sort for obj.name
+            data.sort((a, b) => a.name.localeCompare(b.name));
+            return this.showAllCharacters(data);
+        } catch(error) {
+            document.getElementById('allCharacters').textContent = 'Error fetching character data';
+            console.error(error);
+        }
+    }
+
+    async loadCharacterImage(name) {
+        try {
+            //let lower = await name.toLowerCase();
+            // Maybe take /icon instead of card and use card for their individual page?
+            let response = await fetch(`https://genshin.jmp.blue/characters/${name}/icon-big`);
+
+            if (!response.ok) {
+                return null;
+            }
+
+            let imageBlob = await response.blob();
+            let objectUrl = URL.createObjectURL(imageBlob);
+            return objectUrl;
+        } catch(error) {
+            console.error(error);
+            return null;
+        }
+    }
+
+    async showAllCharacters(results) {
+        const allCharacters = document.getElementById("allCharacters");
+        // TODO: add this to name header once I add html <h1><a href=“../public/character.hmtl”></a></h1>
+
+        const charMap = await Promise.all(results.map(async (char) => {
+            let image = await this.loadCharacterImage(char.id);
+            if (image != null) {
+            return `
+                <div>
+                    <h1> ${char.name}  </h1>
+                    <img src="${image}" alt="${char.name} icon"/>
+                    <h3> ${char.nation} </h3>
+                    <h3> ${char.rarity} </h3>
+                    <h3> ${char.vision} </h3>
+                    <h3> ${char.weapon} </h3>
+                </div>
+                <br>
+                `;
+            }
+            return '';
+        }));
+        allCharacters.innerHTML = charMap.filter(Boolean).join('');
+        return allCharacters;
 
     }
 
@@ -27,6 +82,11 @@ class GenshinApi {
     async getCharacterByRegion(region) {
 
     }
+
+    /*
+        Future: sort characters in front page by nation, weapon, vision, etc (dropdown)
+    */
 }
 
 const genshinApi = new GenshinApi();
+genshinApi.getAllCharacters();
--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
+const path = require('path');
 const app = express();
 const port = 8000;
 
 app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'app')));
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
-    res.send("Hello!");
+    res.render("index");
 })
 
 app.listen(port, () => {
