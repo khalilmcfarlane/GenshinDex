fc66b629e7d515b8d852f329c51bbf9c24c06d25
Khalil McFarlane <kmac36960@gmail.com>
2024-09-23 20:59:14 -0700

    Edited files to contain ejs templating for variables

--- app/js/genshin.js
+++ app/js/genshin.js
@@ -1,6 +1,5 @@
 class GenshinApi {
     #url = "https://gsi.fly.dev/";
-    body = document.body;
     constructor() {
     }
 
@@ -8,11 +7,24 @@ class GenshinApi {
         return this.#url;
     }
 
+    async testAllChar() {
+        // This is how function should be, need to also add image though.
+        try {
+            const response = await fetch(`https://genshin.jmp.blue/characters/all?lang=en`);
+            const data = await response.json();
+            // add a sort for obj.name
+            data.sort((a, b) => a.name.localeCompare(b.name));
+            return data;
+        } catch(error) {
+            document.getElementById('allCharacters').textContent = 'Error fetching character data';
+            console.error(error);
+        }
+    }
+
     async getAllCharacters() {
         try {
-            let response = await fetch(`https://genshin.jmp.blue/characters/all?lang=en`);
-            let data = await response.json();
-            console.log(data);
+            const response = await fetch(`https://genshin.jmp.blue/characters/all?lang=en`);
+            const data = await response.json();
             // add a sort for obj.name
             data.sort((a, b) => a.name.localeCompare(b.name));
             return this.showAllCharacters(data);
@@ -22,11 +34,9 @@ class GenshinApi {
         }
     }
 
-    async loadCharacterImage(name) {
+    async loadCharacterIcon(id) {
         try {
-            //let lower = await name.toLowerCase();
-            // Maybe take /icon instead of card and use card for their individual page?
-            let response = await fetch(`https://genshin.jmp.blue/characters/${name}/icon-big`);
+            let response = await fetch(`https://genshin.jmp.blue/characters/${id}/icon-big`);
 
             if (!response.ok) {
                 return null;
@@ -44,9 +54,10 @@ class GenshinApi {
     async showAllCharacters(results) {
         const allCharacters = document.getElementById("allCharacters");
         // TODO: add this to name header once I add html <h1><a href=“../public/character.hmtl”></a></h1>
+        // TODO: add event lister onmouseover shake div
 
         const charMap = await Promise.all(results.map(async (char) => {
-            let image = await this.loadCharacterImage(char.id);
+            let image = await this.loadCharacterIcon(char.id);
             if (image != null) {
             return `
                 <div>
@@ -67,7 +78,54 @@ class GenshinApi {
 
     }
 
-    async getCharacter(character) {
+    async getCharacterbyName(name) {
+        try {
+            const response = await fetch(`https://genshin.jmp.blue/characters/${name}?lang=en`);
+            const data = response.json();
+            return this.showCharacter(data); 
+
+        } catch (error) {
+            console.error(`Error when retrieving ${name}'s data.`, error);
+        }
+    }
+
+    async loadCharacterCard(id) {
+        try {
+            let response = await fetch(`https://genshin.jmp.blue/characters/${id}/card`);
+            if (!response.ok) {
+                return null;
+            }
+
+            let imageBlob = await response.blob();
+            let objectUrl = URL.createObjectURL(imageBlob);
+            return objectUrl;
+            
+        } catch (error) {
+            console.error("Failed to load card.", error);
+            return null;
+        }
+    }
+
+    async showCharacter(characterJson) {
+        const character = document.getElementById("character");
+        const image = await this.loadCharacterCard(characterJson.id);
+        let result = '';
+        if (image != null) {
+            result = 
+             `
+                <div>
+                    <h1> ${characterJson.name}  </h1>
+                    <img src="${image}" alt="${characterJson.name} icon"/>
+                    <h3> ${characterJson.nation} </h3>
+                    <h3> ${characterJson.rarity} </h3>
+                    <h3> ${characterJson.vision} </h3>
+                    <h3> ${characterJson.weapon} </h3>
+                </div>
+                <br>
+            `;
+        }
+        character.innerHTML = result;
+        return character;
 
     }
 
@@ -89,4 +147,16 @@ class GenshinApi {
 }
 
 const genshinApi = new GenshinApi();
-genshinApi.getAllCharacters();
+//document.addEventListener("DOMContentLoaded", genshinApi.getAllCharacters());
+if (typeof module === 'object') {
+    module.exports = {
+        testAllChar: genshinApi.testAllChar.bind(genshinApi),
+        getCharacterbyName: genshinApi.getCharacterbyName.bind(genshinApi)
+    }
+}
+/*
+module.exports = {
+    testAllChar: genshinApi.testAllChar.bind(genshinApi),
+    getCharacterbyName: genshinApi.getCharacterbyName.bind(genshinApi)
+};
+*/
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require('path');
+const { testAllChar, getCharacterbyName } = require("./app/js/genshin");
 const app = express();
 const port = 8000;
 
@@ -7,8 +8,18 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'app')));
 app.set('views', path.join(__dirname, 'views'));
 
-app.get('/', (req, res) => {
-    res.render("index");
+app.get('/', async (req, res) => {
+    const allCharacters = await testAllChar();
+    //console.log(allCharacters);
+    res.render("index", {allCharacters});
+})
+
+
+app.get('/characters/:characterName', (req, res) => {
+    // Send in character id to character.ejs
+    const name = req.params[characterName].toLowerCase();
+    const characterDetails = getCharacterbyName(name);
+    res.render("character", {characterDetails});
 })
 
 app.listen(port, () => {
